929ebe9b3d3c83a466469e2bdb191dbeb4200a3f
Paulo Benatto <paulobenatto02@gmail.com>
2024-10-08 19:49:43 -0300

    initial structure

--- hello.py
+++ hello.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from organia-challenge!")
+from transformers import pipeline
 
+sentiment_analyzer = pipeline("sentiment-analysis", model="nlptown/bert-base-multilingual-uncased-sentiment", device=0)
+review = """
+O serviço foi muito demorado e o atendente
+parecia completamente despreparado. Precisei
+repetir meu problema várias vezes, e mesmo
+assim senti que ele não estava entendendo o
+que eu estava dizendo. Perdi muito tempo, e o
+pior de tudo é que o problema não foi resolvido
+ao final. Vou reconsiderar continuar usando esse
+serviço.
+"""
+result = sentiment_analyzer(review)
 
-if __name__ == "__main__":
-    main()
+print(result)
--- main.py
+++ main.py
@@ -10,6 +10,23 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/reviews")
+def classify_reviews() -> Union[str, int]:
+    return "positive"
+
+
+@app.get("/reviews")
+def get_reviews() -> Union[str, list]:
+    return ["review1", "review2"]
+
+
+@app.get("/reviews/report")
+def get_reviews_report(start_date: str, end_date: str):
+    return {"start_date": start_date, "end_date": end_date}
+
+
+@app.get("/reviews/{review_id}")
+def get_review(review_id: int):
+    return {"review_id": review_id}
+
+
