c77ef5ec27c375c602474106a3c53adf377fe49b
Paulo Benatto <paulobenatto02@gmail.com>
2024-10-15 22:40:47 -0300

    fix: cors problem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import SeedDatabase
 from routes.reviews import router as reviews_router
@@ -13,6 +14,14 @@ app = FastAPI(
     root_path=f"/api/{API_VERSION}",
     title="OrganIA Challenge",
     description="Desafio OrganIA",
+)
+# noinspection PyTypeChecker
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 app.include_router(reviews_router, prefix="/reviews", tags=["reviews"])
 
